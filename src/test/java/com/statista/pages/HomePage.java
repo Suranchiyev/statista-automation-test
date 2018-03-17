87a9a11b9b21392b9e2dde17fe230c99f787e1c8
Suranchiyev <Suranchiyev96@gmail.com>
2018-03-16 20:36:46 -0400

    added new WebElements in homePage

--- src/test/java/com/statista/pages/HomePage.java
+++ src/test/java/com/statista/pages/HomePage.java
@@ -1,7 +1,27 @@
 package com.statista.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.statista.utilities.Driver;
 import com.statista.utilities.TestBase;
 
 public class HomePage extends TestBase {
     
+	private WebDriver driver;
+	
+	public HomePage() {
+		this.driver = Driver.getDriver();
+		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(id = "logoLink")
+	public WebElement logo;
+	
+	@FindBy(id = "homepageSearch")
+	public WebElement searchBox;
+	
+	
 }
