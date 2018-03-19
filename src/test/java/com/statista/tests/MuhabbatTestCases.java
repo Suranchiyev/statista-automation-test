cdcaced93f8fe78de8445134631e27bab2453ff7
batiky <mhbtkhaydarova@gmail.com>
2018-03-19 15:34:04 -0400

    few lines of assertion

--- src/test/java/com/statista/tests/MuhabbatTestCases.java
+++ src/test/java/com/statista/tests/MuhabbatTestCases.java
@@ -1,7 +1,22 @@
 package com.statista.tests;
 
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+import com.statista.pages.HomePage;
 import com.statista.utilities.TestBase;
 
 public class MuhabbatTestCases extends TestBase{
 
+	@Test
+	public void homePageTitleAndLogoVerification(){
+		HomePage homePage = new HomePage(); 
+	
+	assertTrue(homePage.isAt());
+	assertTrue(homePage.logo.isDisplayed());
+	assertTrue(homePage.searchBox.isDisplayed());
+	
+//		
+}
 }
