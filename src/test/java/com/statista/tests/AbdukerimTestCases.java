369b4920c22bdd6f4a32bee785ae7a3915b9effb
Practice Makes Perfect. <aabdusemi@gmail.com>
2018-03-25 00:09:54 -0400

    Initial commit

--- src/test/java/com/statista/tests/AbdukerimTestCases.java
+++ src/test/java/com/statista/tests/AbdukerimTestCases.java
@@ -1,5 +1,28 @@
 package com.statista.tests;
 
-public class AbdukerimTestCases {
+import static org.testng.Assert.assertTrue;
+import org.testng.annotations.Test;
+import com.statista.pages.HomePage;
+import com.statista.pages.ResultPage;
+import com.statista.utilities.TestBase;
+
+public class AbdukerimTestCases extends TestBase {
+
+	@Test
+	public void searchEmpty() throws InterruptedException {
+		HomePage homePage = new HomePage();
+		// Leave empty search box and click on Search button
+		homePage.searchButton.click();
+		ResultPage resultPage = new ResultPage();
+		// Set in country filter "Afghanistan"
+		resultPage.selectCountryInFilter("Afghanistan");
+		// refresh
+		resultPage.refreshBtn.click();
+		// Verify "Afghanistan is selected"
+		assertTrue(resultPage.getCountry("Afghanistan").isDisplayed());
+		// Select North America
+		Thread.sleep(2000);
+		resultPage.selectRegionByText("North America");
+	}
 
 }
